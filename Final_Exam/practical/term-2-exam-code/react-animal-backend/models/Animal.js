c0ba5377fccb85f5d054f0e552a0fa2ea1ab8241
Yury Shkoda <shkoda.yuri@gmail.com>
2019-12-18 14:07:35 -0500

    ds

--- Final_Exam/practical/term-2-exam-code/react-animal-backend/models/Animal.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const AnimalSchema = new mongoose.Schema({
-    type: {type: String, default: ''},
-});
-
-module.exports = mongoose.model('animal', AnimalSchema);
